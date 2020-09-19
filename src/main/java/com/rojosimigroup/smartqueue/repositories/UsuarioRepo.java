3029a9c17cb6f222641fcef7b1307379f8261f11
ManachoM <manuel.manriquez.b@usach.cl>
2020-09-19 15:18:38 -0300

    Poblada la clase de UsuarioRepo

--- src/main/java/com/rojosimigroup/smartqueue/repositories/UsuarioRepo.java
+++ src/main/java/com/rojosimigroup/smartqueue/repositories/UsuarioRepo.java
@@ -1,6 +1,15 @@
 package com.rojosimigroup.smartqueue.repositories;
 
-public class UsuarioRepo {
+import com.rojosimigroup.smartqueue.models.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+
+@Repository
+public interface UsuarioRepo extends MongoRepository<Usuario, String>{
+
+    Usuario findUserByRut(String rut);
+    List<Usuario> findByDiscapacidad(Boolean disc);
 
 }
