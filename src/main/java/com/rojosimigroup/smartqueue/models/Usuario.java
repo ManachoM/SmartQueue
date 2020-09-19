43a51922a64cc3cf92c145b1642f4af1c77251e8
ManachoM <manuel.manriquez.b@usach.cl>
2020-09-19 15:29:22 -0300

    Se pobla la clase Usuario

--- src/main/java/com/rojosimigroup/smartqueue/models/Usuario.java
+++ src/main/java/com/rojosimigroup/smartqueue/models/Usuario.java
@@ -1,4 +1,68 @@
 package com.rojosimigroup.smartqueue.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("usuario")
 public class Usuario {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String rut;
+    private String telefono;
+    private boolean discapacidad;
+
+
+    public Usuario(String nombre, String apellido, String rut, String telefono, boolean discapacidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+        this.telefono = telefono;
+        this.discapacidad = discapacidad;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public boolean isDiscapacidad() {
+        return discapacidad;
+    }
+
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDiscapacidad(boolean discapacidad) {
+        this.discapacidad = discapacidad;
+    }
 }
