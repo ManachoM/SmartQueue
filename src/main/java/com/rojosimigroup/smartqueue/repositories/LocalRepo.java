d0ec9e690b36d4b6a0b801925468bee4f82787ca
ManachoM <manuel.manriquez.b@usach.cl>
2020-09-19 15:45:19 -0300

    Se pobla la clase LocalRepo

---
+++ src/main/java/com/rojosimigroup/smartqueue/repositories/LocalRepo.java
@@ -0,0 +1,13 @@
+package com.rojosimigroup.smartqueue.repositories;
+
+import com.rojosimigroup.smartqueue.models.Local;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocalRepo extends MongoRepository<Local, String> {
+
+    Local findByNombreLocal(String nombreLocal);
+    Local findByDireccion(String direccion);
+
+}
