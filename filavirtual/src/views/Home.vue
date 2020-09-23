8a16935ac9cb0c8bc78cf82ec0920640877beab6
José <jose.guerra.g@usach.cl>
2020-09-22 21:36:05 -0300

    avance front

---
+++ filavirtual/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+
+    HelloWorld
+  },
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    console.log("holaaa")
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+
+
+}
+</script>
