3fdc7a3460f236009205570c1da4e36b039974c1
Isaac <isaac.rojas@usach.cl>
2020-09-24 01:01:35 -0300

    vista pass (sin back)

--- src/main/resources/smartqueuefront/src/views/Pass.vue
+++ src/main/resources/smartqueuefront/src/views/Pass.vue
@@ -1,8 +1,33 @@
 <template class = "background-color-page">
   <div id="pass">
+    <v-row >
+      <v-col cols = "4">
 
-    <p>hola soy pass</p>
-    <p v-for = "cliente in clientes" :key="`user-name-${cliente.id}`"> {{cliente.nombre}} {{cliente.apellido}}</p>
+      </v-col>
+      <v-col cols = "4" md = "4" align-self>
+        <p class = "fuente-cliente" v-for = "cliente in clientes" :key="`user-name-${cliente.id}`"> {{cliente.nombre}}</p>
+      </v-col>
+
+      <v-col cols = "4">
+
+      </v-col>
+    </v-row>
+    <v-spacer>
+    </v-spacer>
+    <v-row>
+      <v-col cols = "4">
+
+      </v-col>
+      <v-col cols = "4" md = "4">
+        <div >
+          <v-btn class = "alinear-centro" depressed x-large color="#284b63" dark>¡Hacer pasar cliente!</v-btn>
+        </div>
+      </v-col>
+      <v-col cols = "4">
+
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
@@ -34,4 +59,9 @@ export default {
   background-color: #d9d9d9;
   height: 100%;
 }
+.fuente-cliente{
+  font-size: 150%;
+  border:solid #2c3e50 ;
+}
+
 </style>
