a16cad2a1b0d8273c29de5f7b12589405b7d8d0c
ManachoM <manuel.manriquez.b@usach.cl>
2020-09-23 23:40:26 -0300

    Cambios en vista Search.vue

--- src/main/resources/smartqueuefront/src/views/Search.vue
+++ src/main/resources/smartqueuefront/src/views/Search.vue
@@ -1,7 +1,16 @@
 <template class = "background-color-page">
   <div id="search">
-
-    <p>hola soy search</p>
+    <v-card>
+      <v-card-title> <h1 class="display-1"> Busca tu lugar en la fila </h1> </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="RUT" pretend-icon = "mdi-account-circle" />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn>Buscar mi lugar en la fila</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -14,8 +23,8 @@ export default {
 </script>
 
 <style>
-.background-color-page{
+/*.background-color-page{
   background-color: #d9d9d9;
   height: 100%;
-}
+}*/
 </style>
